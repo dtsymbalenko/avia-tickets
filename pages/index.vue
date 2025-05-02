f8961ce8aa47607ef3a73c4ca2b01e77f94308b4
Denys Tsymbalenko <den.cimbal@gmail.com>
2025-05-02 09:45:56 +0300

    Add Homepage template

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <main class="container">
+  <main class="main">
     <NuxtLink to="/" class="logo">
       <NuxtImg class="logo__img" src="/images/logo.svg" alt="Logo" />
     </NuxtLink>
-    <h1>Test</h1>
+
+    <div class="container">
+      <Filters class="container__filter" />
+
+      <div class="container__tickets">
+        <Tabs />
+        <div v-if="loading">Loading...</div>
+
+        <TicketCard />
+
+        <button v-if="!loading" @click="loadMore" class="load-btn">
+          Завантажити ще 5 квитків
+        </button>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
-.container {
+.main {
   max-width: 786px;
   width: 100%;
   padding: 50px 16px;
@@ -23,6 +37,24 @@
   &__img {
     width: 100%;
     height: auto;
+  }
+}
+
+.container {
+  display: flex;
+  align-items: flex-start;
+  gap: 20px;
+
+  &__filter {
+    max-width: 232px;
+    width: 100%;
+  }
+
+  &__tickets {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
   }
 }
 </style>
