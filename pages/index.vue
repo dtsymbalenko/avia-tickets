fa12a1656eabcb476b7546eff44fda8ec037bb66
Denys Tsymbalenko <den.cimbal@gmail.com>
2025-05-02 19:45:00 +0300

    Improvements

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,8 @@ startLoadingTickets()
         <Tabs :activeTab="activeTab" @update:activeTab="(val) => activeTab = val" />
         <div v-if="loading">Loading...</div>
 
-        <LazyTicketCard v-for="ticket in visibleTickets" :key="ticket.price + ticket.carrier + Math.random()"
+        <LazyTicketCard v-for="ticket in visibleTickets"
+          :key="`${ticket.carrier}-${ticket.price}-${ticket.segments[0].origin}-${ticket.segments[0].destination}`"
           :ticket="ticket" />
 
         <h2 v-if="!visibleTickets.length && !loading" class="container__empty">Квитків не знайдено</h2>
@@ -97,6 +98,7 @@ startLoadingTickets()
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #0c7cd5;}
+    background-color: #0c7cd5;
+  }
 }
 </style>
